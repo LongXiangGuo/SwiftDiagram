191ac9050fefac7e0f2322cec21560b14e5f0fb5
jiodg45 <jiodg45@163.com>
2026-08-09 11:20:01 +0800

    Update formula sha256 for v1.0.0 release

--- Formula/swiftdiagram.rb
+++ Formula/swiftdiagram.rb
@@ -2,7 +2,7 @@ class Swiftdiagram < Formula
   desc "Generate PlantUML class diagrams from Swift source code with a local web console"
   homepage "https://github.com/LongXiangGuo/SwiftDiagram"
   url "https://github.com/LongXiangGuo/SwiftDiagram/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_SHA256"
+  sha256 "79e90f0dd65f57091ea47e13f994fd9baf69e3291498735482666f6f9aa475fe"
   license "MIT"
 
   depends_on :macos
